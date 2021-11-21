677b88e11e813f3d332e2f78b45068a4d12ae613
Wallace Myem <wallacemyem@hotmail.com>
2021-11-21 08:45:53 +0100

    first commit

---
+++ src/com/mfa/frames/FXMLDocumentController.java
@@ -0,0 +1,23 @@
+package com.mfa.frames;
+
+import com.mfa.util.ConnectionUtil;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class FXMLDocumentController implements Initializable {
+    
+}
