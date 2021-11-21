0c2bfeb05a4b930a611817941274c10f2088db5c
Wallace Myem <wallacemyem@hotmail.com>
2021-11-21 09:37:12 +0100

    fix javafx

--- src/com/mfa/frames/FXMLDocumentController.java
+++ src/com/mfa/frames/FXMLDocumentController.java
@@ -19,5 +19,63 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class FXMLDocumentController implements Initializable {
+    @FXML
+    private TextField textEmail;
     
+    @FXML
+    private PasswordField textPassword;
+    
+    Stage dialogStage = new Stage();
+    Scene scene;
+    
+    Connection connection = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    
+    public FXMLDocumentController() {
+    connection = ConnectionUtil.connectdb();
+    }
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    String email = textEmail.getText().toString();
+    String password = textPassword.getText().toString();
+    
+    String sql = "SELECT * FROM employee WHERE email = ? and password = ?";
+    
+    try{
+    preparedStatement = connection.prepareStatement(sql);
+    preparedStatement.setString(1, email);
+    preparedStatement.setString(2, password);
+    resultSet = preparedStatement.executeQuery();
+    if(!resultSet.next()){
+    infoBox("Enter Correct Email and Password", "Failed", null);
+    }else{
+    infoBox("Login Successfull", "Success", null);
+    Node source = (Node) event.getSource();
+    dialogStage = (Stage) source.getScene().getWindow();
+    dialogStage.close();
+    scene = new Scene(FXMLLoader.load(getClass().getResource("FXMLMenu.fxml")));
+    dialogStage.setScene(scene);
+    dialogStage.show();
+    }
+    
+    }catch(Exception e){
+    e.printStackTrace();
+    }
+    }
+    
+    public static void infoBox(String infoMessage, String titleBar, String headerMessage)
+    {
+    Alert alert = new Alert(AlertType.INFORMATION);
+    alert.setTitle(titleBar);
+    alert.setHeaderText(headerMessage);
+    alert.setContentText(infoMessage);
+    alert.showAndWait();
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    
+    }
 }
